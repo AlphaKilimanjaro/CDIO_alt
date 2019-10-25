c3b3fd7ca34862c6f20d8de19cf827f2c3676a78
Alfkil Thorbjørn Wennermark <alfkil@gmail.com>
2019-10-25 12:00:41 +0200

    Version 1. Make GUI work, all requirements met, fully OOP. Maven support for GUI added. Added files (again).

---
+++ src/main/java/DTU/Gruppe40/Main.java
@@ -0,0 +1,10 @@
+package DTU.Gruppe40;
+import gui_main.GUI;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        DiceGame game = new DiceGame();
+        game.playDiceGame();
+    }
+}
