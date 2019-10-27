bb7ae1f2e669ef75b323a4d4bffa6b59c56f8e4f
Vialian <dotasoren@gmail.com>
2019-10-27 12:38:26 +0100

    StartUpData implementeret + ændringer på Tile og Board

--- src/main/java/DTU/Gruppe40/Board.java
+++ src/main/java/DTU/Gruppe40/Board.java
@@ -2,10 +2,26 @@ package DTU.Gruppe40;
 
 public class Board {
 
-    Tile tile = new Tile();
+//    Tile tile = new Tile();
+    Tile tiles[];
+
     Board(int num)
     {
-        tile.getMoney(num);
-        tile.getText(num);
+//        tile.getMoney();
+//        tile.getText();
+        tiles[num].getName();
+        tiles[num].getText();
+        tiles[num].getMoney();
+    }
+
+
+
+    public void AddTile(String _name, String _text, int _money)
+    {
+        for (int i = 0; i <= tiles.length; i++)
+        {
+            if (i > tiles.length)
+                tiles[i] = new Tile(_name, _text, _money);
+        }
     }
 }
--- src/main/java/DTU/Gruppe40/Tile.java
+++ src/main/java/DTU/Gruppe40/Tile.java
@@ -3,13 +3,28 @@ package DTU.Gruppe40;
 public class Tile {
 
 
-    String text [] = new String[] {"test1","test2","test3","test4","test5","test6","test7","test8", "test9","test10","test11","test12"};
-    int money [] = new int[] {250,100,100,-20,180,0,-70,60,-80,-50,650};
+    String name = "";
+    String text = "";
+    int money = 0;
 
 
-    public void setText(String _text, int placement) {text[placement] = _text; }
-    public void setMoney(int _money, int placement) { money[placement] = _money; }
+    public Tile(String _name, String _text, int _money)
+    {
+        name = _name;
+        text = _text;
+        money = _money;
+    }
+//    String text [] = new String[] {"test1","test2","test3","test4","test5","test6","test7","test8", "test9","test10","test11","test12"};
+//    int money [] = new int[] {250,100,100,-20,180,0,-70,60,-80,-50,650};
 
-    public String getText(int placement) { return text[placement]; }
-    public int getMoney(int placement) { return money[placement]; }
+
+//    public void setText(String _text, int placement) {text[placement] = _text; }
+//    public void setMoney(int _money, int placement) { money[placement] = _money; }
+//
+//    public String getText(int placement) { return text[placement]; }
+//    public int getMoney(int placement) { return money[placement]; }
+
+    public String getName() { return name; }
+    public String getText() { return text; }
+    public int getMoney() { return money; }
 }
