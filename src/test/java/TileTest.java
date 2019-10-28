ef9b020435dbc792aa72bb728f2c7adef3c87140
Vialian <dotasoren@gmail.com>
2019-10-28 18:01:36 +0100

    Board + boardtest Lavet test som gennemføres (virkende board)

---
+++ src/test/java/TileTest.java
@@ -0,0 +1,36 @@
+import DTU.Gruppe40.Tile;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TileTest {
+
+    Tile tile;
+    @BeforeEach
+    void setUp() {
+        this.tile = new Tile("test1",  "test1+1",  500);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+
+        assertEquals("test1", tile.getName());
+    }
+
+    @Test
+    void getText() {
+        assertEquals("test1+1", tile.getText());
+    }
+
+    @Test
+    void getMoney() {
+        assertEquals(500, tile.getMoney());
+    }
+}
